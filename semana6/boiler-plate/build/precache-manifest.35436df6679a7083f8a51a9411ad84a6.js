5be799fab6911e2466235594c6b21df08e370765
jonatan machado <jonatanmachado35@gmail.com>
2020-05-19 16:54:40 -0300

    projeto feito

---
+++ semana6/boiler-plate/build/precache-manifest.35436df6679a7083f8a51a9411ad84a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3039316ba3c0c0ce92a98ebd320d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1700380b274e20b946",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "e52c62cc381d0f0bd633",
+    "url": "/static/js/2.ef26d66d.chunk.js"
+  },
+  {
+    "revision": "1b1700380b274e20b946",
+    "url": "/static/js/main.e9fa7d46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
