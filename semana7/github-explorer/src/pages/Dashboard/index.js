c1545e2ddcab932555e962f0e6209f9058a9a6a8
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-05 11:13:42 -0300

    projeto com dashboard pronto

--- semana7/github-explorer/src/pages/Dashboard/index.js
+++ semana7/github-explorer/src/pages/Dashboard/index.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 function Dashboard() {
-  return <div />;
+  const [newRepo, setNewRepo] = useState('');
+  const { repositories, setRepositories } = useState([]);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Explore Github" />
+      <Title>Explore repositórios no Github</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button>Pesquisar</button>
+      </Form>
+      <Repositories>
+        {repositories.map((repository) => (
+          <div>
+            <a key={repository.full_name} href="#">
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+            </a>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </div>
+        ))}
+      </Repositories>
+    </>
+  );
 }
 
 export default Dashboard;
--- semana7/github-explorer/src/styles/global.js
+++ semana7/github-explorer/src/styles/global.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github-background.svg';
+
 export default createGlobalStyle`
 *{
   margin:0;
@@ -8,11 +10,21 @@ export default createGlobalStyle`
   box-sizing: border-box;
 }
 body{
-  background: #f0f0f5;
+  background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
   -webkit-font-soothing:antialiased
 }
 
 body, input, button{
-  font: 16px Roboto sans-serif;
+  font: 16px Roboto, sans-serif;
+}
+
+#root{
+  max-width:960px;
+  margin:0 auto;
+  padding:40px 20px;
+}
+
+button{
+  cursor: pointer;
 }
 `;
