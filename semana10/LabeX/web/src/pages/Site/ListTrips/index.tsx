bc8795f5b4d9cf2ab9e70175cfa20d9879769460
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-29 00:23:21 -0300

    list page editada

--- semana10/LabeX/web/src/pages/Site/ListTrips/index.tsx
+++ semana10/LabeX/web/src/pages/Site/ListTrips/index.tsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 
 import api from '../../../services/api';
 import { VerifyLogged } from '../../../utils/VerifyLogged';
-
-// import { Container } from './styles';
+import logoLabeX from '../../../assets/images/logo.png';
+import {
+  ContainerListTripPage,
+  ContainerMediumList,
+  Text,
+  ContainerList,
+  LabeXHeader,
+  Header,
+  HeaderButtons,
+} from './styles';
 
 const ListTrips: React.FC = () => {
   const [listTrips, setListTrips] = useState([]);
@@ -19,34 +27,49 @@ const ListTrips: React.FC = () => {
 
   let lists: string[] = listTrips;
   return (
-    <>
-      <table>
-        <thead>
-          <th />
-          <th>Nome da Viagem</th>
-          <th>Planeta</th>
-          <th>Data</th>
-          <th>Descrição</th>
-          <th>Duração em dias (Terra)</th>
-        </thead>
-        <tbody>
-          {lists.map((trip: any) => (
-            <tr>
-              <td>{trip.name}</td>
-              <td>{trip.planet}</td>
-              <td>{trip.date}</td>
-              <td>{trip.description}</td>
-              <td>{trip.durationInDays}</td>
-              <td>
-                <Link to={`/admin/detalhes-da-viagem/${trip.id}`}>
-                  Ver Candidatos
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <ContainerListTripPage>
+      <Header>
+        <LabeXHeader src={logoLabeX} alt="Logo-LabeX" />
+        <HeaderButtons>
+          <Link to="/admin">
+            <button>Voltar</button>
+          </Link>
+        </HeaderButtons>
+      </Header>
+      <ContainerMediumList>
+        <Text>
+          <h1>Viagens Disponíveis</h1>
+        </Text>
+        <ContainerList>
+          <table>
+            <thead>
+              <th />
+              <th>Nome da Viagem</th>
+              <th>Planeta</th>
+              <th>Data</th>
+              <th>Descrição</th>
+              <th>Duração em dias (Terra)</th>
+            </thead>
+            <tbody>
+              {lists.map((trip: any) => (
+                <tr>
+                  <td>{trip.name}</td>
+                  <td>{trip.planet}</td>
+                  <td>{trip.date}</td>
+                  <td>{trip.description}</td>
+                  <td>{trip.durationInDays}</td>
+                  <td>
+                    <Link to={`/admin/detalhes-da-viagem/${trip.id}`}>
+                      Ver Candidatos
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </ContainerList>
+      </ContainerMediumList>
+    </ContainerListTripPage>
   );
 };
 
