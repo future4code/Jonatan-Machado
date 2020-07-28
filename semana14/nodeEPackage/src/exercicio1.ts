f1aa295c497a0680c2b5f825214d360e7061875e
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-28 20:03:27 -0300

    chore: completed exercises

--- semana14/nodeEPackage/src/exercicio1.ts
+++ semana14/nodeEPackage/src/exercicio1.ts
@@ -7,9 +7,20 @@ que siga a seguinte estrutura:
 */
 
 export const PegaNomeEIdade = (name: string, age: number) => {
+  if (name === '') {
+    const erro = 'Error for leaving empty name';
+    console.log(erro);
+    return erro;
+  }
+  if (isNaN(age)) {
+    const erro = 'Error for passing string at age';
+    console.log(erro);
+    return erro;
+  }
   const response = `olá, ${name}! você tem ${age} anos. Em sete anos você terá ${
     age + 7
   }`;
+  console.log(name, age);
   return response;
 };
 PegaNomeEIdade(process.argv[2], Number(process.argv[3]));
