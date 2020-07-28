ff2931b30987cf35b15be2e545373413041582de
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-28 00:28:17 -0300

    chore: completed functions sum and subtraction

---
+++ semana14/introdutionTS/src/exercicio5.ts
@@ -0,0 +1,7 @@
+export const sum = (number1: number, number2: number): Number => {
+  return number1 + number2;
+};
+
+export const subtraction = (number1: number, number2: number): Number => {
+  return number1 - number2;
+};
