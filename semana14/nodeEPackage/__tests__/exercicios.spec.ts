5bd4e72a0d0cb17a2e47f83d624b7297a838b94a
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-28 20:03:45 -0300

    test: completed exercises

--- semana14/nodeEPackage/__tests__/exercicios.spec.ts
+++ semana14/nodeEPackage/__tests__/exercicios.spec.ts
@@ -1,7 +1,23 @@
 import { PegaNomeEIdade } from '../src/exercicio1';
 import { chooseMathematicaOperation } from '../src/exercicio2';
+import { createTask } from '../src/exercicio3';
+
 describe('Exercicio 1', () => {
-  it('this should show the name entered and the age seven years from now', () => {
+  it('this should show error for leaving empty name', () => {
+    const Name = '';
+    const Age = 27;
+    const result = PegaNomeEIdade(Name, Age);
+    expect(result).toBe('Error for leaving empty name');
+  });
+
+  it('should show error for passing string at age', () => {
+    const Name = 'jonatan';
+    const Age = NaN;
+    const result = PegaNomeEIdade(Name, Age);
+    expect(result).toBe('Error for passing string at age');
+  });
+
+  it('should show the name entered and the age seven years from now', () => {
     const Name = 'Jonatan';
     const Age = 27;
     const result = PegaNomeEIdade(Name, Age);
@@ -31,5 +47,20 @@ describe('exercicio2', () => {
   it('should divide two numbers', () => {
     const result = chooseMathematicaOperation('div', 4, 2);
     expect(result).toBe(2);
+  });
+});
+
+describe('exercicio3', () => {
+  it('should show error for passing empty parameter', () => {
+    const result = createTask('', 'comprar leite');
+    expect(result).toBe('empty parameter error');
+  });
+  it('should show error for passing undefined parameter', () => {
+    const result = createTask(undefined, 'comprar leite');
+    expect(result).toBe('undefined parameter error');
+  });
+  it('should show message done', () => {
+    const result = createTask('tarefa.txt', 'comprar leite');
+    expect(result).toBe('feito');
   });
 });
