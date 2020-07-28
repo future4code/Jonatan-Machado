07b353598b75a61a5cde8dea51a592c547b4e3d7
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-28 00:36:51 -0300

    chore:  function largest number of two numbers

--- semana14/introdutionTS/src/exercicio5.ts
+++ semana14/introdutionTS/src/exercicio5.ts
@@ -5,3 +5,17 @@ export const sum = (number1: number, number2: number): Number => {
 export const subtraction = (number1: number, number2: number): Number => {
   return number1 - number2;
 };
+
+export const multiplication = (number1: number, number2: number): Number => {
+  return number1 * number2;
+};
+
+export const biggestNumber = (number1: number, number2: number): Number => {
+  if (number1 > number2) {
+    return number1;
+  } else if (number1 === number2) {
+    return number1;
+  } else {
+    return number2;
+  }
+};
